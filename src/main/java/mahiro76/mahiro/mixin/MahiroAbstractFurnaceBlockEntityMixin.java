a88a365d1e84ad3f5a99f8116776598ecaa8751a
misaka13210 <misaka13210@gmail.com>
2025-01-18 21:01:11 +0800

    添加燃料Mixin，使木棍可以在熔炉内燃烧。

---
+++ src/main/java/mahiro76/mahiro/mixin/MahiroAbstractFurnaceBlockEntityMixin.java
@@ -0,0 +1,27 @@
+package mahiro76.mahiro.mixin;
+
+import mahiro76.mahiro.registry.MahiroItems;
+import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
+import net.minecraft.item.Item;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Map;
+
+//燃料mixin
+@Mixin(AbstractFurnaceBlockEntity.class)
+public abstract class MahiroAbstractFurnaceBlockEntityMixin {
+
+    //添加燃料时间
+    @Inject(method = "createFuelTimeMap", at = @At("RETURN"), cancellable = true)
+    private static void addFuelItems(CallbackInfoReturnable<Map<Item, Integer>> cir) {
+        Map<Item, Integer> fuelTimes = cir.getReturnValue();
+        //在这里添加物品及其燃料时间（单位：tick）
+        fuelTimes.put(MahiroItems.CRUTCH, 300);
+        cir.setReturnValue(fuelTimes);
+    }
+}
